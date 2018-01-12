d91715aa2db7fda1ffcf32b1c7a6c7911e7689bd
Matias Bjarland <matias@iteego.com>
2018-01-12 15:59:05 +0100

    further cleanup of example code

--- src/leiningen/new/cli_cmd/src/clj_command_line/cli.clj
+++ src/leiningen/new/cli_cmd/src/clj_command_line/cli.clj
@@ -3,21 +3,25 @@
       [clojure.tools.cli :refer [parse-opts]])
     (:gen-class))
 
+;; read https://github.com/clojure/tools.cli
+;; for further details on option parsing
 (def cli-options
-  [;; First three strings describe a short-option, long-option with optional
-   ;; example argument description, and a description. All three are optional
-   ;; and positional.
+  [;; First three strings describe:
+   ;;   1. short-option
+   ;;   2. long-option with optional example argument description
+   ;;   3. description
+   ;; All three are optional and positional.
    ["-p" "--port PORT" "Port number"
     :default 80
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-H" "--hostname HOST" "Remote host"
+   ["-H" "--hostname HOST" "Remote host"                    ; in real life, we would parse to an InetAddress
     :default (str "localhost")
     ;; Specify a string to output in the default column in the options summary
     ;; if the default value's string representation is very ugly
     :default-desc "localhost"]
-   ;; If no required argument description is given, the option is assumed to
-   ;; be a boolean option defaulting to nil
+    ;; If no required argument description is given, the option is assumed to
+    ;; be a boolean option defaulting to nil
    [nil "--detach" "Detach from controlling process"]
    ["-v" nil "Verbosity level; may be specified multiple times to increase value"
     ;; If no long-option is specified, an option :id must be given
@@ -28,39 +32,58 @@
    ["-h" "--help"]])
 
 (defn usage [options-summary]
-      (->> ["This is my program. There are many like it, but this one is mine."
-            ""
-            "Usage: {{{name}}} [options] action"
-            ""
-            "Options:"
-            options-summary
-            ""
-            "Actions:"
-            "  start    Start a new server"
-            "  stop     Stop an existing server"
-            "  status   Print a server's status"
-            ""
-            "Please refer to the manual page for more information."]
-           (string/join \newline)))
+  (string/join
+    \newline
+    ["This is my program. There are many like it, but this one is mine."
+     ""
+     "Usage: {{{name}}} [options] action"
+     ""
+     "Options:"
+     options-summary
+     ""
+     "Actions:"
+     "  start    Start a new server"
+     "  stop     Stop an existing server"
+     "  status   Print a server's status"
+     ""
+     "Please refer to the manual page for more information."]))
 
 (defn error-msg [errors]
-      (str "The following errors occurred while parsing your command:\n\n"
-           (string/join \newline errors)))
+  (str "Errors while parsing command line args:\n\n"
+       (string/join \newline errors)))
+
+(defn command-ok?
+  "make sure the 'command' part of the command line is correct"
+  [arguments]
+  (and (= 1 (count arguments))
+       (#{"start" "stop" "status"} (first arguments))))
+
+(defn invalid-action [summary arguments]
+  (str (usage summary)
+       \newline \newline
+       "Error: invalid action '" (first arguments) "' specified!"))
 
 (defn validate-args
-      "Validate command line arguments. Either return a map indicating the program
-      should exit (with a error message, and optional ok status), or a map
-      indicating the action the program should take and the options provided."
-      [args]
-      (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-           (cond
-             (:help options) {:exit-message (usage summary) :ok? true} ; help => exit OK with usage summary
-             errors          {:exit-message (error-msg errors)}      ; errors => exit with description of errors
-             (and (= 1 (count arguments))                   ; custom validation on arguments
-                  (#{"start" "stop" "status"} (first arguments)))
-                             {:action (first arguments) :options options}
-             :else           {:exit-message (usage summary)})))       ; failed custom validation => exit with usage summary
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with a error message or optional ok status), or a map
+  indicating the action the program should take and the options provided."
+  [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
+        cmd (first arguments)]
+    (println "---- debug output, remove for production code ----")
+    (println "options   " (pr-str options))
+    (println "arguments " (pr-str arguments))
+    (println "errors    " (pr-str errors))
+    (println "summary   " \newline summary)
+    (println "--------------------------------------------------")
+    (cond
+      (:help options)         {:exit-message (usage summary) :ok? true}  ; help => exit OK with usage summary
+      errors                  {:exit-message (error-msg errors)}         ; errors => exit with description of errors
+      (command-ok? arguments) {:action cmd :options options}             ; custom validation on arguments
+      :else                   {:exit-message
+                               (invalid-action summary arguments)})))    ; failed custom validation => exit with usage summary
 
 (defn exit [status msg]
-      (println msg)
-      (System/exit status))
+  (println msg)
+  (System/exit status))
+
--- src/leiningen/new/cli_cmd/src/clj_command_line/core.clj
+++ src/leiningen/new/cli_cmd/src/clj_command_line/core.clj
@@ -6,8 +6,12 @@
 (defn -main [& args]
   (let [{:keys [action options exit-message ok?]} (cli/validate-args args)]
     (if exit-message
-      (cli/exit (if ok? 0 1) exit-message)
+      (exit (if ok? 0 1) exit-message)
+      ;; the options at this point will be a map on the format
+      ;; {:port 80, :hostname "localhost", :verbosity 0}
+      ;; where the values are parsed rich objects if the command line specification
+      ;; chooses to use :parse-fn 
       (case action
-        "start"  (println "starting server with: " options)
-        "stop"   (println "stopping server with: " options)
+        "start" (println "starting server with: " options)
+        "stop" (println "stopping server with: " options)
         "status" (println "status   server with: " options)))))
