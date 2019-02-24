5c9c1ace00888df4e5e8b7dba6e5ccd1ea454736
Matias Bjarland <matias@iteego.com>
2019-02-24 13:02:27 +0100

    project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cli-cmd/lein-template "0.1.3"
+(defproject cli-cmd/lein-template "0.1.4"
   :description "A leiningen template for creating command line executables in clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mbjarland/lein-cli-cmd-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
